41cebc3a69ddac2a2e307b0cc2b00166ebc693a1
Maria Temponera <marakitempo@yahoo.gr>
2024-09-03 15:32:27 +0300

    pokemon-details.js create

--- pokemon.js
+++ pokemon.js
@@ -1,4 +1,4 @@
-const MAX_POKEMON = 649;
+const MAX_POKEMON = 151;
 const listWrapper = document.querySelector(".list-wrapper");
 const searchInput = document.querySelector("#search-input");
 const numberFilter = document.querySelector("#number");
